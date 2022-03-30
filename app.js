1e574af6f03b87d93074b8e2755adbbb60548d26
Michael <michaelf6988@gmail.com>
2022-03-30 15:46:56 +0100

    sounds added no css

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-let audio = new audio()
-
 const boom = document.getElementById("boom");
 const clap = document.getElementById("clap");
 const hihat = document.getElementById("hihat");
@@ -11,5 +9,53 @@ const tink = document.getElementById("tink");
 const tom = document.getElementById("tom");
 
 boom.addEventListener("click", () =>{
-    audio.src = "boom.wav"
+    audioBoom.src = "boom.wav"
 })
+clap.addEventListener("click", () =>{
+    audioClap.src = "clap.wav"
+})
+hihat.addEventListener("click", () =>{
+    audioHihat.src = "hihat.wav"
+})
+kick.addEventListener("click", () =>{
+    audioKick.src = "kick.wav"
+})
+openhat.addEventListener("click", () =>{
+    audioOpenHat.src = "openhat.wav"
+})
+ride.addEventListener("click", () =>{
+    audioRide.src = "ride.wav"
+})
+snare.addEventListener("click", () =>{
+    audioSnare.src = "snare.wav"
+})
+tink.addEventListener("click", () =>{
+    audioTink.src = "tink.wav"
+})
+tom.addEventListener("click", () =>{
+    audioTom.src = "tom.wav"
+})
+
+document.addEventListener("keypress", (action) => {
+    if (action.key === "a" || action.key === "A") {
+           audioBoom.src = "boom.wav";
+    } 
+      else if (action.key === "s" || action.key === "S") {
+            audioClap.src = "clap.wav";
+    }
+      else if (action.key === "d" || action.key === "D") {
+        audioHihat.src = "hihat.wav";
+    }
+      else if (action.key === "f" || action.key === "F") {
+            audioKick.src = "kick.wav";
+    } else if (action.key === "g" || action.key === "G") {
+        audioOpenHat.src = "openhat.wav";
+    } else if (action.key === "h" || action.key === "H") {
+        audioRide.src = "ride.wav";
+    } else if (action.key === "j" || action.key === "J") {
+        audioSnare.src = "snare.wav";
+    } else if (action.key === "k" || action.key === "K") {
+        audioTink.src = "tink.wav";
+    } else if (action.key === "l" || action.key === "L") {
+        audioTom.src = "tom.wav";
+    }})    
